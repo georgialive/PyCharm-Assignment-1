498b70fa2c5899d554e0e2d4547687b36d522f14
Georgia <georgia.m.vrana@gmail.com>
2024-08-13 08:14:38 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Programmer: Georgia Marie Vrana
+# Assignment_1
+# Demonstrate PyCharm Installation and describe setup and programming experience.
+
+firstName = input("what is your name? ")
+lastName = input("what is your last name? ")
+platform = input("What is the type of computer you work on(Windows, Macintosh, Linux)? ")
+language = input("What programming language did you ise in your grade 11 programming course? ")
+print(f"My name is {firstName}{lastName}, I use a {platform} computer!")
+print(f"I used to program in {language} in Grade 11 3U Computer Science.")
+
+
+# Hi there! I know you said to use PyCharm however, my school asked us to use VSCode as our school board
+# does not support the download of PyCharm. I have used VSCode adnauseam, so there shouldn't be any issues
+# on my end. 
